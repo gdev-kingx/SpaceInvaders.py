784c3b0b2c4ee54ff129726c4047af92abde5145
R Sarveshvarn <151063233+gdev-kingx@users.noreply.github.com>
2023-11-20 00:18:44 +0530

    Add files via upload

---
+++ code/laser.py
@@ -0,0 +1,19 @@
+from typing import Any
+import pygame as pg
+
+class Laser(pg.sprite.Sprite):
+    def __init__(self,pos,speed,s_height):
+        super().__init__()
+        self.image = pg.Surface((4, 20))
+        self.image.fill('white')
+        self.rect = self.image.get_rect(center = pos)
+        self.speed = speed
+        self.height_y_constraint = s_height
+
+    def destroy(self):
+        if self.rect.y <= -50 or self.rect.y >= self.height_y_constraint + 50:
+            self.kill()
+
+    def update(self):
+        self.rect.y += self.speed
+        self.destroy()
